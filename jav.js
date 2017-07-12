d4cecaf8dff85a12e1574f64f9696af8bffa2c9d
Ikram Qabiyev <ikram20011127@gmail.com>
2017-07-12 16:23:34 +0400

    corrected all the problems

--- jav.js
+++ jav.js
@@ -3,21 +3,22 @@ var imgDiv = document.getElementsByClassName("team");
 var plus = 0;
 
 function goRight() {
-    
+    plus += 2*imgDiv[0].clientWidth;
     imgLent.style.right = plus + "px"
      
     if (parseFloat(imgLent.style.right) > 2*parseFloat(imgDiv[0].clientWidth)) {
         imgLent.style.right = 0 + "px"
         plus = 0;
     }
+    else {
     plus += 2*imgDiv[0].clientWidth;
-   
+    }
 }
 
 function goLeft() 
 {
     if (imgLent.style.right == "0px") {
-        plus = 1018;
+        plus = 2*parseFloat(imgDiv[0].clientWidth);
     }
     else {
         plus = 0;
